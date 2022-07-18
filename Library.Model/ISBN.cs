1f8a2d3d277584a1031ff247ce999572cf3eea71
SleepyReaper <SleepyReaper@DESKTOP-5BTP1T0>
2022-07-19 01:36:18 +0300

    Add project files.

---
+++ Library.Model/ISBN.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class ISBN
+    {
+        private const int _prefix = 978;
+        public static Dictionary<string, int> Countries { get; set; }
+        public static Dictionary<string, int> Publishers { get; set; }
+
+        private string _country;
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                if (!Countries.ContainsKey(value))
+                    throw new IsbnException($"Country '{value}' is not recognized by ISBN");
+                _country = value;
+            }
+        }
+
+        private string _publisher;
+        public string Publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                if (!Publishers.ContainsKey(value))
+                    throw new IsbnException($"Publisher '{value}' is not recognized by ISBN");
+                _publisher = value;
+            }
+        }
+        public int SerialNumber { get; set; }
+
+        public int Control { get { return (Countries[Country] + Publishers[Publisher] + SerialNumber) % 10; } }
+
+        public override string ToString()
+        {
+            return $"{_prefix}-{Countries[Country].ToString("D3")}-{Publishers[Publisher].ToString("D3")}-{SerialNumber.ToString("D3")}-{Control}";
+        }
+    }
+
+    public class IsbnException : Exception
+    {
+        public IsbnException(string message)
+            : base(message) { }
+    }
+}
---
+++ Library.Model/Journal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Journal : LibraryItem
+    {
+        public static List<string> JournalGanres = new List<string>();
+
+        public List<string> Contributers { get; private set; }
+        public List<string> Editors { get; private set; }
+        public JournalFrequency Frequency { get; set; }
+        public List<string> Ganres { get; private set; }
+
+        public Journal(string title, DateTime publishDate)
+            : base(title, publishDate)
+        {
+            Contributers = new List<string>();
+            Editors = new List<string>();
+            Ganres = new List<string>();
+        }
+    }
+
+    public enum JournalFrequency
+    {
+        Daily,
+        Weekly,
+        BiWeekly,
+        Monthly,
+        BiMonthly,
+        Qurterly,
+        SemiAnnualy,
+        Annualy,
+        BiAnnualy,
+        Other
+    }
+}
+
